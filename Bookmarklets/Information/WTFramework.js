635b20d9d7c1d0891914c8c3a57c56157be205a3
Scott Buchanan <buchanan.sc@gmail.com>
2012-02-21 23:05:42 -0800

    no message

--- Bookmarklets/Information/WTFramework.js
+++ Bookmarklets/Information/WTFramework.js
@@ -1,1 +1,123 @@
-(function(){el=document.getElementById("__wtframework");if(el){document.body.removeChild(el);return }var c=document.createElement("a");c.id="__wtframework";c.style.opacity="0.7";c.style.filter="alpha(opacity=70)";c.style.position="fixed";c.style.zIndex="9000";c.style.top="15px";c.style.right="20px";c.style.background="#000";c.style.styleFloat="right";c.style.padding="7px 10px";c.style.color="#fff";c.style.border="solid 2px #fff";c.style.textDecoration="none";c.style.textAlign="left";c.style.font="12px Lucida Grande,Helvetica,Tahoma";c.style.MozBorderRadius="5px";c.style.WebkitBorderRadius="5px";c.style.WebkitBoxShadow="0px 0px 20px #000";c.style.MozBoxShadow="0px 0px 20px #000";c.href="javascript:void(0)";document.body.appendChild(c);c.onclick=function(){document.body.removeChild(c)};var fm="Bummer... Try checking the source code";try{fm=(MooTools?"MooTools ("+MooTools.version+")":false)}catch(d){}try{fm=(YAHOO?"YUI ("+YAHOO.util.Dom.VERSION+")":false)}catch(d){}try{fm="Prototype ("+Prototype.Version+")"}catch(d){}try{fm+="<br/> Script.aculo.us ("+Scriptaculous.Version+")"}catch(d){}try{fm=(jQuery?"jQuery ("+jQuery.fn.jquery+")":false)}catch(d){}try{fm=(dojo?"Dojo Toolkit ("+dojo.version+")":false)}catch(d){}try{fm=(MochiKit?"MochiKit ("+MochiKit.MochiKit.VERSION+")":false)}catch(d){}try{fm=(base2?"Base2 ("+base2.version+")":false)}catch(d){}c.innerHTML=unescape(fm.toString())})();
+(function(){
+	var fwUl, fwLi = [], fwRemove, wtFramework = document.getElementById('_wtframework'), howMany = 0;
+
+	fwRemove = function(){
+		document.body.removeChild(fwUl);
+	};
+
+	if (wtFramework){
+		document.body.removeChild(wtFramework);
+		return;
+	}
+
+	var fwList = {
+		'ActiveJS': 'ActiveSupport',
+		'Base2': 'base2.version',
+		'Clientcide Libraries': 'Clientcide.version',
+		'Crafty': 'Crafty.init',
+		'DHTMLX': 'dhtmlx',
+		'Dojo': 'dojo.version',
+		'Ext JS': 'Ext.version',
+		'Glow': 'glow.VERSION',
+		'JavaScriptMVC': 'steal.fn',
+		'jQuery': 'jQuery.fn.jquery',
+		'jQuery UI': '$.ui.version',
+		'Midori': 'midori.domReady',
+		'MochiKit': 'MochiKit.MochiKit.VERSION',
+		'MooTools A.R.T.': 'ART.version',
+		'MooTools Core': 'MooTools.version',
+		'MooTools More': 'MooTools.More.version',
+		'Processing.js': 'Processing.version',
+		'Prototype': 'Prototype.Version',
+		'Qooxdoo': 'qx.$$libraries.qx.version',
+		'Raphaël': 'Raphael.version',
+		'Rico': 'Rico.Version',
+		'RightJS': 'RightJS.version',
+		'Sammy': 'Sammy.VERSION',
+		'Script.aculo.us': 'Scriptaculous.Version',
+		'Scripty2': 'S2.Version',
+		'Snack': 'snack.v',
+		'SproutCore': 'SC.isReady',
+		'Spry': 'Spry.$',
+		'YUI 3': 'YUI.version',
+		'YUI 2': 'YAHOO.VERSION',
+		'Zepto': 'Zepto',
+		'ZK': 'zk.version'
+	};
+
+	var fwStyleLi = {
+		cursor: 'pointer',
+		textAlign: 'left',
+		padding: '8px 10px',
+		margin: '0 0 5px',
+		listStyle: 'none',
+		font: 'bold 11px sans-serif',
+		backgroundColor: 'rgba(0, 0, 0, 0.7)',
+		color: '#fff',
+		MozBorderRadius: '5px',
+		webkitBorderRadius: '5px',
+		borderRadius: '5px',
+		borderTop: 'solid 1px rgba(255, 255, 255, 0.4)',
+		borderLeft: 'solid 1px rgba(0, 0, 0, 0.8)',
+		borderRight: 'solid 1px rgba(0, 0, 0, 0.8)',
+		borderBottom: 'solid 1px #000',
+		textShadow: '0 1px 0 #000',
+		MozBoxShadow: '0 -1px 0 #000',
+		webkitBoxShadow: '0 -1px 0 #000',
+		boxShadow: '0 -1px 0 #000',
+		cssFloat: 'right',
+		clear: 'both'
+	};
+
+	var fwStyleUl = {
+		position: 'fixed',
+		padding: '0',
+		margin: '0',
+		right: '10px',
+		top: '10px',
+		zIndex: 300000
+	};
+
+	var props = {
+		onclick: fwRemove,
+		id: '_wtframework'
+	};
+
+	fwUl = document.createElement('ul');
+
+	for (var prop in props){
+		fwUl[prop] = props[prop];
+	}
+
+	for (var s in fwStyleUl){
+		fwUl.style[s] = fwStyleUl[s];
+	}
+
+	document.body.appendChild(fwUl);
+
+	var showInfo = function(fwName, fwVersion){
+		fwLi = document.createElement('li');
+		fwLi.innerHTML = fwName + ((fwVersion && (fwVersion != '%build%')) ? ' (' + fwVersion + ')' : '');
+		for (var s in fwStyleLi){
+			fwLi.style[s] = fwStyleLi[s];
+		}
+		fwUl.appendChild(fwLi);
+	};
+
+	for (var fwNs in fwList){
+		if (fwList.hasOwnProperty(fwNs)){
+			var exists = window;
+			for (var i = 0, idents = fwList[fwNs].split('.'); i < idents.length; i++){
+				exists = exists && exists[idents[i]];
+			}
+			if (exists){
+				showInfo(fwNs, exists.toString());
+				howMany++;
+			}
+		}
+	}
+
+	if (!howMany){
+		showInfo('No known framework detected.');
+	}
+})();
