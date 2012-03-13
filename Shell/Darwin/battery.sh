e1811f932a0f4d10d39918da44b5098519c7187a
Scott Buchanan <buchanan.sc@gmail.com>
2012-03-13 02:33:55 -0700

    no message

--- Shell/Darwin/battery.sh
+++ Shell/Darwin/battery.sh
@@ -5,6 +5,6 @@ battery() {
 	ioreg -w0 -l |
 		grep -E '(Max|Current)Capacity' |
 		perl -pe 's/^[\s\|]*"(\w*)Capacity" = (.*?)[\s]*$/$2 /gi' |
-		awk '{CONVFMT="%.1f" }	{print (($2 / $1 * 100) "%")}'
+		awk '{printf "%.1f%%", ($2 / $1 * 100)}'
 }
 battery
