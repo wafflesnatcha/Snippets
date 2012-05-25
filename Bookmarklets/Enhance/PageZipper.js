59b49fd31d7d8627c67a377854f3fa5b166e344f
Scott Buchanan <buchanan.sc@gmail.com>
2012-05-25 01:08:47 -0700

    no message

--- Bookmarklets/Enhance/PageZipper.js
+++ Bookmarklets/Enhance/PageZipper.js
@@ -1,1 +1,11 @@
-(function(){if(window['pgzp']){_pgzpToggleBookmarklet();}else{window._page_zipper_is_bookmarklet=true;window._page_zipper=document.createElement('script');window._page_zipper.type='text/javascript';window._page_zipper.src='http://www.printwhatyoulike.com/static/pagezipper/pagezipper_10.js';document.getElementsByTagName('head')[0].appendChild(window._page_zipper);}})();
+(function() {
+	if (window['pgzp']) {
+		_pgzpToggleBookmarklet();
+	} else {
+		window._page_zipper_is_bookmarklet = true;
+		window._page_zipper = document.createElement('script');
+		window._page_zipper.type = 'text/javascript';
+		window._page_zipper.src = 'http://www.printwhatyoulike.com/static/pagezipper/pagezipper_10.js';
+		document.getElementsByTagName('head')[0].appendChild(window._page_zipper);
+	}
+})();
--- Bookmarklets/Enhance/bookmaplet.js
+++ Bookmarklets/Enhance/bookmaplet.js
@@ -1,34 +1,34 @@
 (function() {
 	var shadowWidth = 5;
 	var shadowHeight = 17;
-	
+
 	function bookmaplet() {
 		var txt = extractSelection();
-		
+
 		if (txt == "") {
 			alert("Nothing selected");
 			return;
 		}
-		
-		txt = txt.replace(/\r\n|\r|\n/g,", ");
-		txt = encodeURIComponent(txt).replace(/ /g,"+");
-				
-		installViewElements(txt);		
+
+		txt = txt.replace(/\r\n|\r|\n/g, ", ");
+		txt = encodeURIComponent(txt).replace(/ /g, "+");
+
+		installViewElements(txt);
 	}
-		
+
 	function extractSelection() {
 		var txt;
 		if (window.getSelection) {
-			txt = ''+window.getSelection();
+			txt = '' + window.getSelection();
 		} else if (document.selection) {
 			txt = document.selection.createRange().text;
 		} else if (document.selection) {
 			txt += document.selection.createRange().text;
 		}
-		
+
 		return txt;
 	}
-	
+
 	function installViewElements(txt) {
 		var bmp_container = createDiv();
 		bmp_container.id = "bmp_container";
@@ -37,7 +37,7 @@
 		bmp_container.style.right = "0";
 		bmp_container.style.width = "auto";
 		bmp_container.style.zIndex = 100000;
-		
+
 		var bmp_shadow = createDiv(bmp_container);
 		bmp_shadow.id = "bmp_shadow";
 		bmp_shadow.style.backgroundColor = "black";
@@ -46,7 +46,7 @@
 		bmp_shadow.style.top = "0";
 		bmp_shadow.style.right = "0";
 		setOpacity(bmp_shadow, 0.3);
-		
+
 		var bmp_map = createDiv(bmp_container);
 		bmp_map.id = "bmp_map";
 		bmp_map.style.backgroundColor = "white";
@@ -54,35 +54,35 @@
 		bmp_map.style.width = "450px";
 		bmp_map.style.height = "290px";
 		bmp_map.innerHTML = '<iframe src="http://www.bookmaplet.com/mapping?a=' + txt + '&o=' + location.href + '" frameborder="0" id="bmp_iframe" style="width:100%;height:100%;border:0px;padding:0px;margin:0px"></iframe>';
-//		bmp_map.innerHTML = '<iframe src="http://localhost:8888/mapping?a=' + txt + '&o=' + location.href + '" frameborder="0" id="bmp_iframe" style="width:100%;height:100%;border:0px;padding:0px;margin:0px"></iframe>';
+		//		bmp_map.innerHTML = '<iframe src="http://localhost:8888/mapping?a=' + txt + '&o=' + location.href + '" frameborder="0" id="bmp_iframe" style="width:100%;height:100%;border:0px;padding:0px;margin:0px"></iframe>';
 		document.body.appendChild(bmp_container);
 
 		bmp_shadow.style.width = (bmp_map.offsetWidth + shadowWidth) + "px";
 		bmp_shadow.style.height = (bmp_map.offsetHeight + shadowHeight) + "px";
-		
+
 		window.onscroll = function() {
 			bmp_container.style.top = scrollPos().y + "px";
-		};		
-		
+		};
+
 		var closeElement = document.createElement("img");
 		closeElement.style.position = "relative";
 		closeElement.style.left = "405px";
 		closeElement.src = 'http://www.bookmaplet.com/images/close.png';
-//		closeElement.src = 'http://localhost:8888/images/close.png';
+		//		closeElement.src = 'http://localhost:8888/images/close.png';
 		closeElement.onclick = function() {
 			var node = document.getElementById("bmp_container");
 			node.parentNode.removeChild(node);
-		}		
+		}
 		bmp_container.appendChild(closeElement);
-		
+
 		var openingooglemapsElement = document.createElement("a");
 		openingooglemapsElement.style.position = "relative";
 		openingooglemapsElement.style.left = "205px";
 		openingooglemapsElement.style.backgroundColor = "white";
 		openingooglemapsElement.style.foregroundColor = "white";
 		openingooglemapsElement.href = 'http://maps.google.com/maps?q=' + txt;
-		openingooglemapsElement.innerHTML = '<img style="border-style: none" src=\"http://www.bookmaplet.com/images/openingooglemaps.png\">';		
-//		openingooglemapsElement.innerHTML = '<img style="border-style: none" src=\"http://localhost:8888/images/openingooglemaps.png\">';		
+		openingooglemapsElement.innerHTML = '<img style="border-style: none" src=\"http://www.bookmaplet.com/images/openingooglemaps.png\">';
+		//		openingooglemapsElement.innerHTML = '<img style="border-style: none" src=\"http://localhost:8888/images/openingooglemaps.png\">';		
 		bmp_container.appendChild(openingooglemapsElement);
 	}
 
