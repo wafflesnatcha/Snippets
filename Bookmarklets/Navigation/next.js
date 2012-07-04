c1d963918e08db71a0912bc51f8889b2f2593540
Scott Buchanan <buchanan.sc@gmail.com>
2012-07-04 13:38:47 -0700

    no message

--- Bookmarklets/Navigation/next.js
+++ Bookmarklets/Navigation/next.js
@@ -1,4 +1,5 @@
-(function (num, rel) {
+(function (num, rel, pattern) {
+	// try finding a <link rel="next"> tag
 	if (rel) {
 		var i, links = Array.prototype.slice.call(document.getElementsByTagName('link')),
 			l = links.length;
@@ -9,6 +10,21 @@
 			}
 		}
 	}
+
+	// look for links who's text is 'next'
+	if (pattern) {
+		var i, links = document.links,
+			l = links.length,
+			re = new RegExp('\s*' + pattern + '\s*', 'i');
+		for (i = 0; i < l; i++) {
+			if(re.test(links[i].innerText.replace(/[^\w\s]+/, '').replace(/^\s+|\s+$/g, '')) && links[i].href) {
+				document.location = links[i].href;
+				return;
+			}
+		}
+	}
+
+	// increment the last numeric span in the URL
 	window._BMRK_NAV_INC = window._BMRK_NAV_INC || num;
 	var m = document.location.href.match(/^(.*?)(\d+)([^\d]*)$/);
 	if (m) {
@@ -18,4 +34,4 @@
 		}
 		document.location = m[1] + x + m[3];
 	}
-})(1, 'next');
+})(1, 'next', 'next');
