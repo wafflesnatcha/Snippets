721ca62d5860ab1629ef66ef7bb2c3716039d21a
Scott Buchanan <buchanan.sc@gmail.com>
2012-07-24 18:08:05 -0700

    No need for comments in bookmarklets, so: `uglifyjs -nc`

--- Bookmarklets/bookmarklet.sh
+++ Bookmarklets/bookmarklet.sh
@@ -9,9 +9,9 @@ fi
 bin="$(which uglifyjs 2>/dev/null)" || { echo "${0##*/}: uglifyjs required" 1>&2; exit 2; }
 
 if type ruby &>/dev/null; then
-	echo -n "javascript:$(cat "$1" | "$bin" | ruby -pe '$_.gsub!(/([^a-zA-Z0-9_\.\-]+)/n) { "%" + $1.unpack("H2" * $1.size).join("%").upcase }')"
+	echo -n "javascript:$(cat "$1" | "$bin" -nc | ruby -pe '$_.gsub!(/([^a-zA-Z0-9_\.\-]+)/n) { "%" + $1.unpack("H2" * $1.size).join("%").upcase }')"
 elif type php &>/dev/null; then
-	echo -n "javascript:$(cat "$1" | "$bin" | php -r 'echo rawurlencode(file_get_contents("php://stdin"));')"
+	echo -n "javascript:$(cat "$1" | "$bin" -nc | php -r 'echo rawurlencode(file_get_contents("php://stdin"));')"
 else
 	echo "${0##*/}: ruby or php required to URL encode output" 1>&2
 	exit 2
