5809f2bec4ec9f709294adfa9adfdece10383088
Scott Buchanan <buchanan.sc@gmail.com>
2012-08-16 19:30:34 -0700

    no message

--- Bookmarklets/Navigating/next-previous.js
+++ Bookmarklets/Navigating/next-previous.js
@@ -1,8 +1,9 @@
 (function (num, rel, pattern) {
+	var i, links, l, re;
 	// try finding a <link rel="next"> tag
 	if (rel) {
-		var i, links = Array.prototype.slice.call(document.getElementsByTagName('link')),
-			l = links.length;
+		links = Array.prototype.slice.call(document.getElementsByTagName('link'));
+		l = links.length;
 		for (i = 0; i < l; i++) {
 			if (links[i].rel == rel && links[i].href) {
 				window.location = links[i].href;
@@ -11,13 +12,13 @@
 		}
 	}
 
-	// look for links who's text is 'next'
+	// look for links who's text matches a "next"-like pattern
 	if (pattern) {
-		var i, links = document.links,
-			l = links.length,
-			re = new RegExp('([^\w]|^)\s*(' + pattern + ')\s*([^\w]|$)', 'i');
+		links = document.links;
+		l = links.length;
+		re = new RegExp("([^\\w]|^)\\s*(" + pattern + ')\\s*([^\\w]|$)', 'i');
 		for (i = 0; i < l; i++) {
-			if (re.test(links[i].innerText.replace(/[^\w\s]+/, '').replace(/^\s+|\s+$/g, '').replace(/[\n\r]/g, ' ')) && links[i].href) {
+			if (links[i].href && re.test(links[i].innerText.replace(/[^\w\s]+/, '').replace(/^\s+|\s+$/g, '').replace(/[\n\r]/g, ' '))) {
 				window.location = links[i].href;
 				return;
 			}
@@ -25,14 +26,13 @@
 	}
 
 	// increment the last numeric span in the URL
-	window.__BMRK_NAV_INC = window.__BMRK_NAV_INC || num;
-	var m = window.location.href.match(/^(.*?)(\d+)([^\d]*)$/);
-	if (m) {
-		var x = "" + (parseInt(m[2], 10) + window.__BMRK_NAV_INC);
-		while (x.length < m[2].length) {
-			x = "0" + x;
+	l = window.location.href.match(/^(.*?)(\d+)([^\d]*)$/);
+	if (l) {
+		i = parseInt(l[2], 10) + (window.__BMRK_NAV_INC = (window.__BMRK_NAV_INC || 0 ) + num);
+		while (i.length < l[2].length) {
+			i = "0" + i;
 		}
-		window.location = m[1] + x + m[3];
+		window.location = l[1] + i + l[3];
 	}
-// })(-1, 'prev', '(prev|previous)(\s*page)?');
-})(1, 'next', 'next(\s*page)?');
+}(-1, 'prev', '(prev|previous)(\\s*page)?|newer'));
+// }(1, 'next', 'next(\s*page)?|older'));
