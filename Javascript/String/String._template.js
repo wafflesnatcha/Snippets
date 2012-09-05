77152f22c0e88c1f13a19439212d645e5b779a18
Scott Buchanan <buchanan.sc@gmail.com>
2012-09-04 19:55:45 -0700

    no message

--- Javascript/String/String._template.js
+++ Javascript/String/String._template.js
@@ -14,20 +14,18 @@
  * console.log(str.template(data));
  * </code>
  *
- * @param {Object} data The object containing replacement data for the template variables.
+ * @param   {Object} data The object containing replacement data for the template variables.
  * @returns {String}
- * @author Scott Buchanan <buchanan.sc@gmail.com>
- * @link http://wafflesnatcha.github.com
+ * @author  Scott Buchanan <buchanan.sc@gmail.com>
+ * @link    http://wafflesnatcha.github.com
  */
-if (!String.prototype.template) {
-	String.prototype.template = function (data) {
-		var prop, result = this,
-			data = data || {};
-		for (prop in data) {
-			if (data.hasOwnProperty(prop)) {
-				result = result.replace('<%' + prop + '%>', data[prop]);
-			}
+String.prototype._template = function (data) {
+	var prop, result = this;
+	data = data || {};
+	for (prop in data) {
+		if (data.hasOwnProperty(prop)) {
+			result = result.replace('<%' + prop + '%>', data[prop]);
 		}
-		return result.replace(/<%.+?%>/ig, '');
-	};
-}
+	}
+	return result.replace(/<%.+?%>/ig, '');
+};
