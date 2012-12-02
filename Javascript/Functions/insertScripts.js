abc63a92c4a058c5b5841129ebfac362f03892f1
Scott Buchanan <buchanan.sc@gmail.com>
2012-12-01 22:35:07 -0800

    no message

--- Javascript/Functions/insertScripts.js
+++ Javascript/Functions/insertScripts.js
@@ -1,17 +1,22 @@
 /**
  * Incrementally add new javascript file to the document head
  */
+/*jshint browser:true*/
 function insertScripts(urls, callback) {
-	if (typeof urls == 'string') urls = [urls];
-	var thisFn = arguments.callee,
+	if (typeof urls == "string") {
+		urls = [urls];
+	}
+	var thisFn = insertScripts,
 		url = urls.shift(),
-		el = document.createElement('script');
+		el = document.createElement("script");
 	el.src = url;
 	el.type = "text/javascript";
 	el.addEventListener("load", function () {
-		console.log(arguments.callee.toString(), "Script Loaded", url);
-		if (urls.length > 0) thisFn.apply(this, [urls, callback]);
-		else if (callback) callback.apply(this, arguments);
+		if (urls.length > 0) {
+			thisFn.apply(this, [urls, callback]);
+		} else if (callback && typeof callbacl) {
+			callback.apply(this, arguments);
+		}
 	}, false);
-	document.getElementsByTagName('head')[0].appendChild(el);
+	document.getElementsByTagName("head")[0].appendChild(el);
 }
