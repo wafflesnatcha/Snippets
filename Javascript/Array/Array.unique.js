c830c6704f5e114caa2e632c851e6d09e5774ef8
Scott Buchanan <buchanan.sc@gmail.com>
2012-12-26 08:56:36 -0800

    no message

--- Javascript/Array/Array.unique.js
+++ Javascript/Array/Array.unique.js
@@ -1,32 +1,36 @@
 /**
  * Array.unique
- * 
+ *
  * faster
  */
 if (!Array.prototype.unique) {
 	Array.prototype.unique = function () {
-		var obj = {},
-			i = this.length,
-			arr = [],
-			t;
-		while (i--) t = this[i], obj[t] = t;
-		for (i in obj) arr.push(obj[i]);
+		var t, i = this.length,
+			obj = {}, arr = [];
+		while (i--) {
+			t = this[i], obj[t] = t;
+		}
+		for (i in obj) {
+			arr.push(obj[i]);
+		}
 		return arr;
 	};
 }
 
 /**
  * Array.unique
- * 
+ *
  * retains original order
  * modifies source array
  */
 if (!Array.prototype.unique) {
 	Array.prototype.unique = function () {
-		var i, x, l = this.length;
+		var i, x;
 		for (i = 0; i < this.length; i++) {
 			for (x = i + 1; x < this.length; x++) {
-				while (this[i] == this[x]) this.splice(x, 1);
+				while (this[i] == this[x]) {
+					this.splice(x, 1);
+				}
 			}
 		}
 		return this;
