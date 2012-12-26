e54510eb1770d28fe68d5d68571862da42352420
Scott Buchanan <buchanan.sc@gmail.com>
2012-12-26 07:18:16 -0800

    no message

--- Javascript/String/String.leftPad.js
+++ Javascript/String/String.leftPad.js
@@ -7,8 +7,10 @@
  */
 if (!String.prototype.leftPad) {
 	String.prototype.leftPad = function (length, str) {
-		if (this.length >= length) return this;
-		var str = str || ' ';
+		if (this.length >= length) {
+			return this;
+		}
+		str = str || ' ';
 		return (new Array(Math.ceil((length - this.length) / str.length) + 1).join(str)).substr(0, (length - this.length)) + this;
 	};
 }
