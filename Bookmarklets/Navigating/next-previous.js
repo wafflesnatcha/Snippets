12b065e879649325e4245482d385e0b058072de6
Scott Buchanan <buchanan.sc@gmail.com>
2013-02-27 16:06:40 -0800

    no message

--- Bookmarklets/Navigating/next-previous.js
+++ Bookmarklets/Navigating/next-previous.js
@@ -1,43 +1,62 @@
 /*jshint browser:true, devel:true*/
-(function (delta, rel, pattern) {
-	var i, l, links;
+(function (rel, pattern, delta) {
+	var arr, l, m;
 
-	function go(url) {
-		// return console.log(url);
-		return window.location = url;
+	function link(el) {
+		if (el.href) {
+			window.location = el.href;
+		} else {
+			// Try triggering a click event on the element if it has no href
+			if (document.createEvent) {
+				var e = document.createEvent('HTMLEvents');
+				e.initEvent('click', true, true);
+				el.dispatchEvent(e);
+			} else if (document.createEventObject) {
+				el.fireEvent('on' + event, document.createEventObject());
+			}
+		}
+		return;
 	}
 
-	// Method 1: find a <link rel=...> tag
+	// Method 1: find tag with 'rel' attribute
 	if (rel) {
-		links = Array.prototype.slice.call(document.getElementsByTagName('link')).concat(Array.prototype.slice.call(document.links));
-		l = links.length;
-		for (i = 0; i < l; i++) {
-			if (links[i].rel == rel && links[i].href) {
-				return go(links[i]);
+		arr = Array.prototype.slice.call(document.getElementsByTagName('link'));
+		while (arr.length > 0) {
+			l = arr.shift();
+			if (l.rel == rel && l.href) {
+				window.location = l.href;
+			}
+		}
+
+		arr = Array.prototype.slice.call(document.getElementsByTagName('a'));
+		while (arr.length > 0) {
+			l = arr.shift();
+			if (l.rel == rel) {
+				return link(l);
 			}
 		}
 	}
 
 	// Method 2: look for links who's text matches a given regular expression
 	if (pattern) {
-		links = document.links;
-		l = links.length;
 		pattern = new RegExp('([^\\w]|^)\\s*(' + pattern + ')\\s*([^\\w]|$)', 'i');
-		for (i = 0; i < l; i++) {
-			if (links[i].href && pattern.test(links[i].innerText.replace(/^\s+|\s+$/g, '').replace(/[\n\r]/g, ' '))) {
-				return go(links[i]);
+		arr = Array.prototype.slice.call(document.getElementsByTagName('a'));
+		while (arr.length > 0) {
+			l = arr.shift();
+			if (pattern.test(l.innerText.replace(/^\s+|\s+$/g, '').replace(/[\n\r]/g, ' '))) {
+				return link(l);
 			}
 		}
 	}
 
 	// Method 3: increment/decrement the last numeric span in the URL
-	l = window.location.href.match(/^(.*?)(\d+)([^\d]*)$/);
-	if (l) {
-		i = String(parseInt(l[2], 10) + (window.__BMRK_NAV_INC = (window.__BMRK_NAV_INC || 0) + delta));
-		while (i.length < l[2].length) {
-			i = '0' + i;
+	m = window.location.href.match(/^(.*?)(\d+)([^\d]*)$/);
+	if (m) {
+		l = String(parseInt(m[2], 10) + (window.__BMRK_NAV_INC = (window.__BMRK_NAV_INC || 0) + delta));
+		while (l.length < m[2].length) {
+			l = '0' + l;
 		}
-		return go(l[1] + i + l[3]);
+		return window.location = m[1] + l + m[3];
 	}
-}(-1, 'prev', '<+|(prev|previous)(\\s*(page|post))?|newer'));
-// }(1, 'next', '>+|next(\\s*(page|post))?|older'));
+}('prev', '<+|(prev|previous)(\\s*(page|post))?|newer', -1));
+// }('next', '>+|next(\\s*(page|post))?|older', 1));
