7c4a24c23ffe2bf647b1d5ea22946fbd61b4c895
Scott Buchanan <buchanan.sc@gmail.com>
2013-04-06 18:15:42 -0700

    renamed

--- Bookmarklets/TEMPLATES/Quick Search.js
+++ Bookmarklets/TEMPLATES/Quick Search.js
@@ -1,13 +1,14 @@
 /**
- * Firefox Quick Search template
- * 
+ * Quick Search Template
+ *
+ * Firefox:
  * Add as a firefox bookmark and set the keyword. Then whenever you use that
  * keyword in the URL bar, it will go to a different URL depending on if you
  * added a search query after the keyword.
  *
  * Example (adding a suffix to the URL):
  * javascript:location='http://www.youtube.com/'+('%s'?'results?search_query=%s':'')
- * 
+ *
  * Example (two completely different URLs):
  * javascript:location=(('%s')?'https://developer.mozilla.org/search?q=%s':'http://www.mozilla.org/')
  */
